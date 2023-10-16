4aec3cfb7766adab21575bfb2af6df9600384828
AureliusLim <aurelius_justin_lim@dlsu.edu.ph>
2023-10-16 10:18:58 +0800

    initial fcfs

--- fcfs.py
+++ fcfs.py
@@ -1,9 +1,37 @@
 class FCFS:
   def __init__(self, queue):
     self.queue = queue
-
+    self.timeCounter = 0
+    self.totalWaiting = 0
+    self.averageWaiting = 0
   def start(self):
-    print(self.queue[0].pid)
-    print(self.queue[0].arrivalTime)
-    print(self.queue[0].burstTime)
-    # prints all executed processes
+    self.processStart()
+    self.calculateWaitTime()
+    self.printOutput()
+  
+
+  def processStart(self):
+    #sufficient because fcfs is non preemptive so it will finish executing task before moving to the next
+    self.queue.sort(key=lambda x:(int(x.arrivalTime), int(x.pid)))
+
+    for process in range(0, len(self.queue)):
+      self.queue[process].startTime.append(self.timeCounter)
+      self.timeCounter += int(self.queue[process].burstTime)
+      self.queue[process].endTime.append(self.timeCounter)
+      self.queue[process].waitingTime = int(self.queue[process].endTime[0]) - int(self.queue[process].burstTime) - int(self.queue[process].arrivalTime)
+
+  def calculateWaitTime(self):
+    for process in range(0, len(self.queue)):
+      self.totalWaiting += int(self.queue[process].waitingTime)
+
+    self.averageWaiting = self.totalWaiting / len(self.queue)
+
+  def printOutput(self):
+      for x in self.queue:
+          print(f'{x.pid}', end="")
+          for y,z in zip(x.startTime,x.endTime):
+              print(f' start time: {y} end time: {z} |', end="")
+          print(f' Waiting time: {x.waitingTime}')
+
+      
+      print(f'Average waiting time: {self.averageWaiting}')
